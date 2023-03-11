b355cea08786f1347e2df85e6e07b8391d254a97
andrewbocker <58228967+andrewbocker@users.noreply.github.com>
2023-03-11 08:24:44 -0600

    productos

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/Productos.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Productos.java
@@ -10,4 +10,47 @@ package SuperCompreBarato;
  */
 public class Productos {
     
+    private String idProducto;
+    private String descripción;
+    private int precio;
+    private ArrayList[] inventario;
+
+    public Productos(String idProducto, String descripción, int precio) {
+        this.idProducto = idProducto;
+        this.descripción = descripción;
+        this.precio = precio;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public ArrayList[] getInventario() {
+        return inventario;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setInventario(ArrayList[] inventario) {
+        this.inventario = inventario;
+    }
+    
 }
