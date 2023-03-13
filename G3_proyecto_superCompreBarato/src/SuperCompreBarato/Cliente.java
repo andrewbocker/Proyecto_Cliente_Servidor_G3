67881f46c1f79350803b1bdea59691bfc0b2aa92
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-12 19:43:17 -0600

    Update Cliente.java

---
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SuperCompreBarato;
+
+/**
+ *
+ * @author Minor
+ */
+public class Cliente extends Persona {
+     int numOrden;
+    String nombreProduc;
+    
+    
+}
