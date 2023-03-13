dd8e6c7e6dd3dea92130f831f607875e4f0e1163
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-12 19:43:59 -0600

    Create Proveedor.java

---
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Proveedor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SuperCompreBarato;
+
+/**
+ *
+ * @author andre
+ */
+public class Proveedor {
+    
+    private String idProvedor;
+    private String proveedor;
+
+    public Proveedor() {
+    }
+
+    public String getIdProvedor() {
+        return idProvedor;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setIdProvedor(String idProvedor) {
+        this.idProvedor = idProvedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+}
