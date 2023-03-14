661c172190b305c286c259480a06ef5e841b6957
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-14 10:02:07 -0600

    Update Inventario.java

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/Inventario.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Inventario.java
@@ -12,7 +12,17 @@ public class Inventario {
 
     public void añadirProducto(String nombreProducto, String idProducto, String descripcion, double precio, int stack) {
 
-        // CREA UN NUEVO PRODUCTO Y LO AÑADE A LA LISTA
+        // VERIFICA PRIMERO SI EXISTE UN PRODUCTO EN LA LISTA CON EL MISMO ID
+        if (buscarProducto(idProducto) != -1)
+        {
+
+            // SI EXISTE UN PRODUCTO CON EL MISMO ID, AVISA AL USUARIO Y NO AÑADE EL PRODUCTO EN LA LISTA
+            System.out.println("No se puede añadir varios productos con un mismo id.");
+            return;
+
+        }
+
+        // DE LO CONTRARIO CREA UN NUEVO PRODUCTO Y LO AÑADE A LA LISTA
         Producto producto = new Producto(nombreProducto, idProducto, descripcion, precio, stack);
         listInventario.add(producto);
         System.out.println("Se ha añadido correctamente el nuevo Producto");
@@ -21,38 +31,23 @@ public class Inventario {
 
     public void modificarProducto(String nombreProducto, String idProducto, String descripcion, double precio, int stack) {
 
-        // SE CREA UNA VARIABLE "index"  PARA SABER EN QUE POSICION DE LA LISTA NOS ENCONTRAMOS
-        int index = 0;
+        // BUSCA LA POSICION DEL OBJETO EN LA LISTA
+        int index = buscarProducto(idProducto);
 
-        // RECORRE LA LISTA
-        for (Producto actProducto : listInventario)
-        {
+        // MODIFICA LOS VALORES DEL PRODUCTO EN LA LISTA
+        listInventario.get(index).setNombreProducto(nombreProducto);
+        listInventario.get(index).setDescripción(descripcion);
+        listInventario.get(index).setPrecio(precio);
+        listInventario.get(index).setStack(stack);
 
-            // VERIFICA EN CADA PRODUCTO EL ID BUSCANDO EL QUE COINCIDA 
-            if (actProducto.getIdProducto().equals(idProducto))
-            {
-
-                // SI ENCUENTRA UN ID COINCIDENTE ENTONCES INTERCAMBIA LOS DATOS ANTIGUOS POR LOS NUEVOS
-                listInventario.get(index).setNombreProducto(nombreProducto);
-                listInventario.get(index).setDescripción(descripcion);
-                listInventario.get(index).setPrecio(precio);
-                listInventario.get(index).setStack(stack);
-
-                System.out.println("El producto con el id = " + idProducto + " ha sido modificado correctamente");
-
-                // LUEGO SALE DEL METODO
-                return;
-
-            }
-
-            // SI EL ID DEL PRODUCTO NO COINCIDE ENTONCES AUMENTA EL "index"  EN 1 Y CONTINUA VERIFICANDO EL SIGUIENTE ELEMENTO EN LA LISTA
-            index++;
-
-        }
+        System.out.println("El producto con el id = " + idProducto + " ha sido modificado correctamente");
 
     }
 
-    public Producto buscarProducto(String idProducto) {
+    public int buscarProducto(String idProducto) {
+
+        // SE CREA UNA VARIABLE "index" PARA SABER EN QUE POSICION DE LA LISTA NOS ENCONTRAMOS
+        int index = 0;
 
         // RECORRE TODA LA LISTA
         for (Producto actProduto : listInventario)
@@ -62,55 +57,72 @@ public class Inventario {
             if (actProduto.getIdProducto().equals(idProducto))
             {
 
-                // SI ENCUENTRA UN ID COINCIDENTE ENTONCES LO RETORNA
-                return actProduto;
+                // SI ENCUENTRA UN ID COINCIDENTE ENTONCES RETORNA SU POSICION EN LA LISTA
+                return index;
 
             }
 
         }
 
-        // SI RETORNA NULL SIGNIFICA QUE NO SE ENCONTRO NINGUN PRODUCTO CON ESE ID
-        return null;
+        // SI RETORNA -1 SIGNIFICA QUE NO SE ENCONTRO NINGUN PRODUCTO CON ESE ID
+        return -1;
+
+    }
+
+    public Producto getProducto(String idProducto) {
+
+        int index = buscarProducto(idProducto);
+
+        // PRIMERO VERIFICAMOS SI EL PRODUCTO EXISTE
+        if (index == -1)
+        {
+
+            // SI NO EXISTE, AVISAMOS AL USUARIO Y RETORNA NULL
+            System.out.println("No se encontro ningun producto con le id = " + idProducto);
+            return null;                       
+            
+        }
+        
+        // DE LO CONTRARIO RETORNA EL PRODUCTO ENCONTRADO EN LA LISTA
+        return listInventario.get(index);
 
     }
 
     public void eliminarProducto(String idProducto) {
 
-        // SE CREA UNA VARIABLE "index"  PARA SABER EN QUE POSICION DE LA LISTA NOS ENCONTRAMOS
-        int index = 0;
+        int index = buscarProducto(idProducto);
 
-        // RECORRE TODA LA LISTA
-        for (Producto actProducto : listInventario)
+        // PRIMERO SE VERIFICA SI EL PRODUCTO EXISTE
+        if (index == -1)
         {
 
-            // VERIFICA EN CADA PRODUCTO EL ID BUSCANDO EL QUE COINCIDA
-            if (actProducto.getIdProducto().equals(idProducto))
-            {
-
-                // SI ENCUENTRA UN ID COINCIDENTE ENTONCES SE REMUEVE EL PRODUCTO ALMACENADO EN EL "index" ACTUAL
-                listInventario.remove(index);
-                System.out.println("Se ha eliminado el producto = " + "" + ", con el id = " + actProducto.getIdProducto());
-
-                // LUEGO SALE DEL METODO
-                return;
-
-            }
-
-            // SI EL ID DEL PRODUCTO NO COINCIDE ENTONCES AUMENTA EL "index"  EN 1 Y CONTINUA VERIFICANDO EL SIGUIENTE ELEMENTO EN LA LISTA
-            index++;
+            // SI NO EXISTE, AVISAMOS AL USUARIO Y SE SALE DEL METODO
+            System.out.println("No se encontro ningun producto con le id = " + idProducto);
+            return;
 
         }
 
-        // SI NO ENCUENTRA UN ID COINCIDENTE ENTONCES SIMPLEMENTE TERMINA LA EJECUCION DEL METODO
-        System.out.println("No se Encontro ningun producto con el id = " + idProducto);
+        // DE LO CONTRARIO, ELIMINAMOS EL PRODUCTO DE LA LISTA
+        System.out.println("Se ha eliminado el producto = " + listInventario.get(index).getNombreProducto() + ", con el id = " + idProducto);
+        listInventario.remove(index);
 
     }
-    
-    public void clearInventario() {
-        listInventario.clear();
+
+    public void restarStackProducto(String idProducto) {
+
+        listInventario.get(buscarProducto(idProducto)).restarStack();
+
     }
-    
+
+    public void clearInventario() {
+
+        listInventario.clear();
+
+    }
+
     public boolean isInventarioEmpty() {
+
         return listInventario.isEmpty();
+
     }
 }
