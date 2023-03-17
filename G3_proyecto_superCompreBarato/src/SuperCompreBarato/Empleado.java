a616bd5bb669551ec5c16bb9e2e481e6ccd7be50
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-16 22:16:52 -0600

    Update Empleado.java

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/Empleado.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Empleado.java
@@ -6,7 +6,40 @@ package SuperCompreBarato;
 
 //autor minor
 public class Empleado extends Persona {
-     int aniosExperiencia;
-     String area,cargo;
-    
+
+    private String idEmpleado;
+    private String puestoTrabajo;
+    private double salario;
+
+    public Empleado(String idEmpleado, String puestoTrabajo, double salario, String nombre, String apellido1, String apellido2, String cedula, int edad, String lugarResidencia) {
+        super(nombre, apellido1, apellido2, cedula, edad, lugarResidencia);
+        this.idEmpleado = idEmpleado;
+        this.puestoTrabajo = puestoTrabajo;
+        this.salario = salario;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getPuestoTrabajo() {
+        return puestoTrabajo;
+    }
+
+    public void setPuestoTrabajo(String puestoTrabajo) {
+        this.puestoTrabajo = puestoTrabajo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
 }
