003b3cd93027d38ff3ae2b6745e7e38b7db9e2d0
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-16 22:16:34 -0600

    Update Persona.java

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/Persona.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Persona.java
@@ -9,8 +9,73 @@ package SuperCompreBarato;
  * @author minor
  */
 public class Persona {
-    int aniosExperiencia;
-     String area,cargo;
+
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String cedula;
+    private int edad;
+    private String lugarResidencia;
+
+    public Persona(String nombre, String apellido1, String apellido2, String cedula, int edad, String lugarResidencia) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.lugarResidencia = lugarResidencia;
+    }
     
-    
+    public String stringNombreCompleto() {
+        return nombre + " " + apellido1 + " " + apellido2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLugarResidencia() {
+        return lugarResidencia;
+    }
+
+    public void setLugarResidencia(String lugarResidencia) {
+        this.lugarResidencia = lugarResidencia;
+    }
+
 }
