46c54bd4211860a62f4c0d868a4950b0d926acec
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-03-18 12:01:46 -0600

    Update Cliente.java

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/Cliente.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Cliente.java
@@ -9,8 +9,45 @@ package SuperCompreBarato;
  * @author Minor
  */
 public class Cliente extends Persona {
-     int numOrden;
-    String nombreProduc;
+     
+    private String idCliente;    
+    private String tipoCliente;
+    private int puntos = 0;
     
+    public static final String[] TIPOS_CLIENTES = {"Casual", "Regular", "VIP"};
+
+    public Cliente(String nombre, String apellido1, String apellido2, String cedula, int edad, String lugarResidencia, String idCliente, String tipoCliente) {
+        super(nombre, apellido1, apellido2, cedula, edad, lugarResidencia);
+        this.idCliente = idCliente;
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
     
+    public void sumarPuntos(int nuevosPuntos){
+        puntos += nuevosPuntos;
+    }
+                
 }
