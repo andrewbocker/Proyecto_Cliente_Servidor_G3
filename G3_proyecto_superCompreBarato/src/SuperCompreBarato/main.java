847f03b75b467cbae169b3fa67ca5b82e3b90dc8
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-04-22 21:27:16 -0600

    Update main.java

--- G3_proyecto_superCompreBarato/src/SuperCompreBarato/main.java
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/main.java
@@ -4,6 +4,8 @@
  */
 package SuperCompreBarato;
 
+import Pantallas.Menu;
+
 /**
  *
  * @author andre
@@ -14,7 +16,8 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu menu = new Menu();
+        menu.setVisible(true);
     }
     
 }
