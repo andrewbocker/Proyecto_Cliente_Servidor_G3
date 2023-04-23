ce66b9d6ac24091c5ccbcffdbac41f42f4ef3b4f
Bryan <61808598+Roxaaas@users.noreply.github.com>
2023-04-22 21:27:34 -0600

    Create Conexion.java

---
+++ G3_proyecto_superCompreBarato/src/SuperCompreBarato/Conexion.java
@@ -0,0 +1,33 @@
+package SuperCompreBarato;
+
+import java.sql.*;
+import javax.swing.*;
+
+public class Conexion {
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/super_compre_barato";
+    private static final String USER = "root";
+    private static final String PASS = "ByW1552000";
+
+    public static Connection getConnection(){
+    
+        Connection con = null;
+        
+        try
+        {
+            
+            con = (Connection)DriverManager.getConnection(URL, USER, PASS);
+            JOptionPane.showMessageDialog(null, "La conexion fue exitosa");
+
+        } catch (Exception e)
+        {
+            
+            JOptionPane.showMessageDialog(null, e);
+            
+        }
+        
+        return con;
+        
+    }
+    
+}
